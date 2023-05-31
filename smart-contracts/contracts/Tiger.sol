0e9693ea5e9b33b87ef76be6d8d4ebf0a865a88b
Son Le <>
2023-05-31 16:27:55 +0700

    Init with dummy tokens

---
+++ smart-contracts/contracts/Tiger.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// SPDX-License-Identifier: UNLICENSED
+
+contract Tiger is ERC20, Ownable {
+    address minter;
+
+    event SetMinter(address _newMinter);
+
+    modifier onlyMinter {
+        require(msg.sender == minter, "only minter");
+        _;
+    }
+
+    constructor() ERC20("Tiger ERC20 token", "TIGER"){}
+
+    function mint(address recipient, uint256 amount) public onlyMinter {
+        _mint(recipient, amount);
+    }
+
+    function setMinter(address _newMinter) public onlyOwner {
+        minter = _newMinter;
+        emit SetMinter(_newMinter);
+    }
+}
