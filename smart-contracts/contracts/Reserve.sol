fb95c4f1974d02fbdd3de04f40a153840df8e89b
Son Le <>
2023-05-31 17:33:59 +0700

    Exchange

--- smart-contracts/contracts/Reserve.sol
+++ smart-contracts/contracts/Reserve.sol
@@ -16,6 +16,8 @@ contract Reserve is Ownable {
 
     event WithdrawFund(address indexed token, address indexed admin, address indexed fundTo, uint256 amount);
     event SetChangeRate(uint256 exchangeRate, uint256 reverseExchangeRate);
+    event ExchangeToQuote(address indexed user, uint256 fromAmt, uint256 toAmount);
+    event ExchangeToBase(address indexed user, uint256 fromAmt, uint256 toAmount);
 
     constructor(address _quoteToken) {
         quoteToken = IERC20(_quoteToken);
@@ -57,5 +59,35 @@ contract Reserve is Ownable {
 
             return to;
         }
+    }
+
+    function exchange(bool isFromBaseToQuote, uint256 amount) payable public {
+       if (isFromBaseToQuote) {
+           // transfer ETH to this smart contract
+           require(msg.value == amount, "Not enough ETH");
+           (bool sent, ) = address(this).call{value: amount}("");
+           require(sent, "Failed to send ETH");
+
+           // transfer quote token for user
+           uint256 to = getExchangeRate(true, amount);
+           require(to > 0, "Not enough quote token liquidity");
+
+           IERC20(quoteToken).transfer(msg.sender, to);
+
+           emit ExchangeToQuote(msg.sender, amount, to);
+       } else {
+           require(amount <= IERC20(quoteToken).balanceOf(msg.sender), "Not enough quote token");
+           uint256 to = getExchangeRate(false, amount);
+           require(to > 0, "Not enough base token liquidity");
+
+           // transfer quote token to this smart contract
+           IERC20(quoteToken).transferFrom(msg.sender, address(this), amount);
+
+           // transfer ETH for user
+           (bool sent, ) = msg.sender.call{value: to}("");
+           require(sent, "Failed to send ETH");
+
+           emit ExchangeToBase(msg.sender, amount, to);
+       }
     }
 }
