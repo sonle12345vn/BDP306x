a10dc3a29d2708d5294a2a47eaf85da5549001e3
Son Le <>
2023-05-31 19:05:23 +0700

    Batch mint

--- smart-contracts/test/test_exchange.js
+++ smart-contracts/test/test_exchange.js
@@ -4,7 +4,11 @@ const {ethers} = require("hardhat")
 describe("Exchange", function () {
     const DECIMALS = 18;
 
-    async function deployAndMintReserve(deployer, tokenContractName) {
+    async function deployAndMintReserve(
+        deployer,
+        tokenContractName,
+        mintFor
+    ) {
         // get factory
         const Factory = await ethers.getContractFactory(tokenContractName);
 
@@ -14,16 +18,18 @@ describe("Exchange", function () {
 
         // mint 1B tokens for deployer
         const mintAmt = new ethers.utils.parseUnits("1000000000", DECIMALS);
-        await token.connect(deployer).mint(deployer.address, mintAmt)
+        for (const addr in [deployer.address].push(mintFor)) {
+            await token.connect(deployer).mint(addr, mintAmt)
+        }
     }
 
-    async function setupAll(admin) {
-        await deployAndMintReserve(admin, "Tiger")
-        await deployAndMintReserve(admin, "Lion")
+    async function setupAll(admin, mintFor) {
+        await deployAndMintReserve(admin, "Tiger", mintFor)
+        await deployAndMintReserve(admin, "Lion", mintFor)
     }
 
     it("Exchange between two ERC20 tokens", async function() {
-        const [admin] = await ethers.getSigners();
-        await setupAll(admin);
+        const [admin, alice, bob] = await ethers.getSigners();
+        await setupAll(admin, [alice.address, bob.address]);
     })
 })
